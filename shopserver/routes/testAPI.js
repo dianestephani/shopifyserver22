548192f943fa4958e0d3f35eaae6b241be299cbb
Diane Stephani <diane.stephani@gmail.com>
2022-01-16 20:21:47 -0800

    routing, json file doesn't work

--- shopserver/routes/testAPI.js
+++ shopserver/routes/testAPI.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+import itemList from './items.json';
 
+//2. Via home route, sends message back to client.
 router.get('/', function(req, res, next) {
-    res.send('API is working properly');
+    res.send(itemList);
 });
 
 module.exports = router;
